eb1a39ec436eea4527f1f25b96b7db32bbf5769c
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-08-26 23:23:49 -0300

    corrigindo erro de espaço

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import '../style/mobilePage.css'
 
 export default function About() {
   return (
     <section 
 id="about"
-style={{  display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", padding: "60px" }}>
+style={{  display: "flex",width:'auto', flexDirection: "column", justifyContent: "center", alignItems: "center", padding: "60px" }}>
         <h1 style={{fontSize:'30px' , marginBottom:"30px"}}>ABOUT ME</h1>
-      <div style={{ width: "600px" , fontSize:'20px', textAlign:"center", lineHeight:'26px'}}>
+      <div
+ style={{  fontSize:'20px', textAlign:"center", lineHeight:'26px' ,}}
+className="aboutText">
         I am 24 years old, a web developer based in Luziânia-Goiás. I have a passion for creating web pages and solving problems. I feel excited when presented with new challenges and I am always seeking to learn something new.
       </div>
     </section >
