77e366dfef539003be2e579196b1963be28524f8
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-08-26 22:56:45 -0300

    adicionando backgrounColor footer e header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <section style={{ textAlign:"center", padding:'20px',background: "#512da8"}}>
+    <section style={{ textAlign:"center", padding:'20px',background: " linear-gradient(300deg, rgb(56, 39, 116) 34%, rgb(105, 96, 228) 78%)"}}>
       <p >Todos direitos reservados. © 2023 <span>Claydson Henrique</span> </p>
     </section>
   )
