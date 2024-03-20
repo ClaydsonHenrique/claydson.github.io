b824ebd39152a9070c6b35bbd5482b5a334645d0
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-03-20 12:02:43 -0300

    corrigindo responsividade

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Hearder from './components/Header'
 import Home from './page/Home'
 import AboutHome from './page/About'
@@ -10,9 +10,8 @@ import './style/style.css'
 
 
 function App() {
-  const [menu, setMenu] = useState(false)
   return (
-    <div style={{ position: 'relative' }} >
+    <div className='containerApp' >
       <Hearder />
       <Home />
       <AboutHome></AboutHome>
--- src/page/About.jsx
+++ src/page/About.jsx
@@ -8,14 +8,16 @@ export default function AboutHome() {
       <Slide triggerOnce>
         <section className='containerAboutHome'>
           {/* mais uma explicação breve */}
-          <div className='container-AH-1'>
-            <p className='about-03'><span className="textBlue">{'<'}</span> ABOUT ME <span className="textBlue">{'| >'}</span></p>
-            <p className='description-03'>Eu iniciei com web desenvolvedor atraves de um curso intensivo, que tem duração de 1 ano e nos da conhecimento tanto front quanto back end. Desde então não paro mais de procurar conhecimeto e explorar novos desafios. </p>
-          </div>
-          <div className='container-AH-1 '>
-            <p className='about-03'><span className="textBlue">{'<'}</span> OBJETIVOS<span className="textBlue">{'| >'}</span></p>
-            <p className=' description-03'>Minha jornada no mundo do desenvolvimento começou em 2022, quando fiz a transição de carreira para seguir minha paixão pela tecnologia e pela criação digital. Desde então, venho explorando esse emocionante universo e aprimorando minhas habilidades como desenvolvedor web para um dia ne tornar um Full Stack.</p>
-          <button className="curriculo">Acesse meu Curriculo</button>
+          <div className="containerRigth">
+            <div className='container-AH-1'>
+              <p className='about-03'><span className="textBlue">{'<'}</span> ABOUT ME <span className="textBlue">{'| >'}</span></p>
+              <p className='description-03'>Eu iniciei com web desenvolvedor atraves de um curso intensivo, que tem duração de 1 ano e nos da conhecimento tanto front quanto back end. Desde então não paro mais de procurar conhecimeto e explorar novos desafios. </p>
+            </div>
+            <div className='container-AH-1 '>
+              <p className='about-03'><span className="textBlue">{'<'}</span> OBJETIVOS<span className="textBlue">{'| >'}</span></p>
+              <p className=' description-03'>Minha jornada no mundo do desenvolvimento começou em 2022, quando fiz a transição de carreira para seguir minha paixão pela tecnologia e pela criação digital. Desde então, venho explorando esse emocionante universo e aprimorando minhas habilidades como desenvolvedor web para um dia ne tornar um Full Stack.</p>
+              <button className="curriculo">Acesse meu Curriculo</button>
+            </div>
           </div>
         </section>
       </Slide>
